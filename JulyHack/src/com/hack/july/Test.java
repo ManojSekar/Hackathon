71cead6bdca7efa1c2802b2a63b1142d1258f664
ManojSekar <manojkumar.sekar@verizon.com>
2015-08-01 03:32:39 -0700

    Test Java File

---
+++ JulyHack/src/com/hack/july/Test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.hack.july;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Test {
+
+	/**
+	 * 
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Project");
+
+	}
+
+}
